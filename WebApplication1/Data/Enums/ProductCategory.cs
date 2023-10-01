2c2425539a2ce5592ad88624424efcb5f935d795
Marcus <marcus.zhang@inossem.com>
2023-09-30 21:25:52 -0400

    E-Commerce Web Application 1.0

---
+++ WebApplication1/Data/Enums/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EStore.Data.Enums
+{
+    public enum ProductCategory
+    {
+        GPU,
+        CPU,
+        Memory,
+        [Display(Name = "Hard Drive")]
+        HardDrive,
+        [Display(Name = "Hard Drive")]
+        VideoWall,
+        [Display(Name = "Encoders & Decoders")]
+        EncodersAndDecoders,
+
+
+
+    }
+}
---
+++ WebApplication1/Migrations/20230924215312_NameFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompanLogoURL",
+                table: "Companies",
+                newName: "CompanyLogoURL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompanyLogoURL",
+                table: "Companies",
+                newName: "CompanLogoURL");
+        }
+    }
+}
